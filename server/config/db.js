d19b90cc1a9cc02345c05d51f172545a882c1099
sgtneckbeard <nathanheung@gmail.com>
2022-10-27 15:29:36 -0400

    cleanup

--- server/config/db.js
+++ server/config/db.js
@@ -6,4 +6,5 @@ https://comp229-f2022midterm-301150982.herokuapp.com/
 
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb+srv://nathan:hjU8uXnVNjOPsGTV@mongodbserver.eorljjx.mongodb.net/book_store?retryWrites=true&w=majority"};
+  "URI": "mongodb+srv://nathan:hjU8uXnVNjOPsGTV@mongodbserver.eorljjx.mongodb.net/book_store?retryWrites=true&w=majority"
+};
